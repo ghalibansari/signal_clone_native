5fa8c8470538fb9fe2d7c5450a4a4ddede11c284
GhalibAansari <ghalibansari1994@gmail.com>
2021-03-07 23:31:34 +0530

    init

---
+++ firebase.tsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAO_XU_C-tyZientIWGUkcRg6c9mC6v150",
+    authDomain: "signal-clone-native-13a5c.firebaseapp.com",
+    projectId: "signal-clone-native-13a5c",
+    storageBucket: "signal-clone-native-13a5c.appspot.com",
+    messagingSenderId: "472952865855",
+    appId: "1:472952865855:web:9b671e8037bb06d51ce48a"
+};
+
+export {}
